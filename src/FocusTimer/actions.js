183d3c20c7975684b1075ee47a49ac88fc63c6f8
horquidia-lima <horquidia.a.lima@gmail.com>
2023-11-07 22:18:28 -0300

    adcionando sons na aplicacao

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,11 +1,13 @@
 import state from './state.js'
 import * as timer from './timer.js'
 import * as el from './elements.js'
+import * as sounds from './sounds.js'
 
 export function toggleRunning() {
   state.isRunning = document.documentElement.classList.toggle('running')
 
   timer.countdown()
+  sounds.buttonPressAudio.play()
 }
 
 export function reset(){
@@ -13,6 +15,7 @@ export function reset(){
   document.documentElement.classList.remove('running')
 
   timer.updateDisplay()
+  sounds.buttonPressAudio.play()
 }
 
 export function set(){
@@ -22,4 +25,10 @@ export function set(){
 
 export function toggleMusic(){
   state.isMute = document.documentElement.classList.toggle('music-on')
+
+  if(state.isMute){
+    sounds.bgAudio.play()
+    return
+  }
+  sounds.bgAudio.pause()
 }
